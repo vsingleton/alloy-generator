834e89b904cee4053e29b855723dc76bb7c400e6
kylestiemann <kyle.stiemann@liferay.com>
2014-04-11 17:24:18 -0400

    Changed Reserved to Namespaced and allowed the namespace to be configured. Also added the ability to handle JSF reserved attributes.

--- generator/src/com/liferay/alloy/util/ReservedAttributeUtil.java
+++ generator/src/com/liferay/alloy/util/ReservedAttributeUtil.java
@@ -14,15 +14,17 @@
 
 package com.liferay.alloy.util;
 
-import com.liferay.alloy.tools.model.Attribute;
-import com.liferay.alloy.tools.model.Component;
-
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import jodd.util.StringPool;
 
 import org.apache.commons.lang.StringUtils;
+
+import com.liferay.alloy.tools.model.Attribute;
+import com.liferay.alloy.tools.model.Component;
 
 /**
  * @author Eduardo Lundgren
@@ -30,16 +32,10 @@ import org.apache.commons.lang.StringUtils;
  */
 public class ReservedAttributeUtil {
 
-	public static final List<String> JAVA_RESERVED_WORDS = Arrays.asList(
-		PropsUtil.getStringArray("builder.java.reserved.words"));
-
-	public static final List<String> RESERVED_ATTRIBUTES = Arrays.asList(
-		PropsUtil.getStringArray("builder.reserved.attributes"));
-
 	public static String getJavaSafeName(Attribute attribute) {
 		String name = attribute.getName();
 
-		name = getSafeName(attribute);
+		name = attribute.getSafeName();
 
 		Component component = attribute.getComponent();
 
@@ -54,6 +50,18 @@ public class ReservedAttributeUtil {
 		return name;
 	}
 
+	public static String getJSFReservedAttributeType(Attribute attribute) {
+
+		String reservedAttributeType = null;
+		String name = attribute.getName();
+
+		if (_isJSFReservedAttribute(name)) {
+			reservedAttributeType = JSF_RESERVED_ATTRIBUTES_MAP.get(name);
+		}
+
+		return reservedAttributeType;
+	}
+
 	public static String getOriginalName(
 		String componentName, String attributeName) {
 
@@ -61,7 +69,7 @@ public class ReservedAttributeUtil {
 			StringUtil.replaceFirst(
 				attributeName, componentName.toLowerCase(), StringPool.EMPTY));
 
-		if (isReserved(originalName)) {
+		if (isNamespaced(originalName)) {
 			attributeName = originalName;
 		}
 
@@ -71,13 +79,19 @@ public class ReservedAttributeUtil {
 	public static String getSafeName(Attribute attribute) {
 		String name = attribute.getName();
 
-		if (isReserved(attribute)) {
-			Component component = attribute.getComponent();
+		if (isNamespaced(attribute)) {
 
-			String componentUncapitalizedName =
-				component.getUncapitalizedName();
 			String capitalizedAttributeName = StringUtils.capitalize(name);
-			name = componentUncapitalizedName.concat(capitalizedAttributeName);
+
+			if (NAMESPACE != null) {
+				name = NAMESPACE.concat(capitalizedAttributeName);
+			} else {
+				Component component = attribute.getComponent();
+
+				String componentUncapitalizedName =
+						component.getUncapitalizedName();
+				name = componentUncapitalizedName.concat(capitalizedAttributeName);
+			}
 		}
 
 		return name;
@@ -87,12 +101,50 @@ public class ReservedAttributeUtil {
 		return JAVA_RESERVED_WORDS.contains(attribute.getName());
 	}
 
-	public static boolean isReserved(Attribute attribute) {
-		return isReserved(attribute.getName());
+	public static boolean isJSFReservedAttribute(Attribute attribute) {
+		return JSF_RESERVED_ATTRIBUTES_MAP.containsKey(attribute.getName());
 	}
 
-	public static boolean isReserved(String attributeName) {
-		return RESERVED_ATTRIBUTES.contains(attributeName);
+	public static boolean isNamespaced(Attribute attribute) {
+		return isNamespaced(attribute.getName());
+	}
+
+	public static boolean isNamespaced(String attributeName) {
+		return NAMESPACED_ATTRIBUTES.contains(attributeName);
+	}
+
+	private static boolean _isJSFReservedAttribute(String attributeName) {
+		return JSF_RESERVED_ATTRIBUTES_MAP.containsKey(attributeName);
+	}
+
+	public static final List<String> JAVA_RESERVED_WORDS = Arrays.asList(
+		PropsUtil.getStringArray("builder.java.reserved.words"));
+
+	public static final Map<String, String> JSF_RESERVED_ATTRIBUTES_MAP;
+
+	public static final String NAMESPACE = PropsUtil.getString("builder.namespace", null);
+
+	public static final List<String> NAMESPACED_ATTRIBUTES = Arrays.asList(
+		PropsUtil.getStringArray("builder.namespaced.attributes"));
+
+	private static final List<String> _JSF_RESERVED_ATTRIBUTES_AND_TYPES = Arrays.asList(
+			PropsUtil.getStringArray("builder.jsf.reserved.attributes"));
+
+	static {
+
+		Map<String, String> jsfReservedAttributesMap = null;
+
+		if (_JSF_RESERVED_ATTRIBUTES_AND_TYPES != null) {
+
+			jsfReservedAttributesMap = new HashMap<String, String>();
+
+			for (String keyValuePair : _JSF_RESERVED_ATTRIBUTES_AND_TYPES) {
+				String[] pair = keyValuePair.split("\\|");
+				jsfReservedAttributesMap.put(pair[0], pair[1]);
+			}
+		}
+
+		JSF_RESERVED_ATTRIBUTES_MAP = jsfReservedAttributesMap;
 	}
 
 }
